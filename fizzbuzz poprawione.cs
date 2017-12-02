1cd7e5a199fb33b68886b232ed14503b35d6a263
lukinysa <33897947+lukinysa@users.noreply.github.com>
2017-12-02 16:38:02 +0100

    Add files via upload

---
+++ fizzbuzz poprawione.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+            int num01 = 3;
+            int num02 = 5;
+            for (int i = 1; i < 101; i++)
+            {
+                Console.WriteLine(i);
+                if (i % num01 == 0)
+                {
+                    Console.WriteLine(i + "Fizz");
+                }
+                if (i % num02 == 0)
+                {
+                    Console.WriteLine(i + "Buzz");
+                }
+                if (i % num01 == 0 && i % num02 == 0)
+                {
+                    Console.WriteLine(i + "FizzBuzz FizzBuzz");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
